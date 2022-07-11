7ac2b5c8948f6db9afd0023f3f60d519b9905f85
Einar <einargudnig@gmail.com>
2022-07-11 12:40:12 +0000

    scraper outputs to csv

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import Head from "next/head";
 import React from "react";
 import styles from "../styles/Home.module.css";
 import nextPackage from "next/package.json";
+// import output from "../scraper/output.csv"
 
-export default function Home({}) {
+export default function Home({ }) {
+  // console.log(output)
   return (
     <div className={styles.container}>
       <Head>
